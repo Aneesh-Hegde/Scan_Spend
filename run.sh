b2db3d2cb6a0e0dc92ab2be6cd04048c230690e5
Aneesh Hegde <aneeshhegde7110@gmail.com>
2025-02-10 23:31:12 +0530

    wait for process to run

--- run.sh
+++ run.sh
@@ -4,15 +4,18 @@
 echo "Starting Go backend..."
 cd backend &&
   go run main.go &
+sleep 5
 
 # Start Next.js frontend
 echo "Starting Next.js frontend..."
 cd frontend &&
   npm run dev &
+sleep 5
 
 # Start Envoy proxy
 echo "Starting Envoy..."
 envoy -c envoy-dev.yaml &
+sleep 5
 
 # Wait for all background processes
 wait
