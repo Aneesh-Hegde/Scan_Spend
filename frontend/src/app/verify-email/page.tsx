07936f3ad53e7b8c287579e434bd4d8e2440238e
Aneesh Hegde <aneeshhegde7110@gmail.com>
2025-02-14 01:50:23 +0530

    solving useSearchParams for build command 😡

--- frontend/src/app/verify-email/page.tsx
+++ frontend/src/app/verify-email/page.tsx
@@ -1,20 +1,28 @@
-"use client"; import { useEffect } from 'react' 
-import { useRouter, useSearchParams } from 'next/navigation' 
-import EmailVerification from '../utils/verify-email'; 
-const VerifyEmail = () => { 
-  const searchParams = useSearchParams() 
-  const router = useRouter()
-  const token: string | null = searchParams.get("token")
-  useEffect(() => {
-      EmailVerification(token)
-        router.push('/')
+"use client"; import { useEffect, Suspense } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import EmailVerification from '../utils/verify-email';
+const VerifyEmail = () => {
+  const VerifyEmailComponent = () => {
 
-  }, [token, router])
+    const searchParams = useSearchParams()
+    const router = useRouter()
+    const token: string | null = searchParams.get("token")
+    useEffect(() => {
+      EmailVerification(token)
+      router.push('/')
+
+    }, [token, router])
+    return (
+      <div style={{ textAlign: "center", padding: "2rem" }}>
+        <h2>Verify Your Email</h2>
+        <p>We’ve sent you a verification email. Please check your inbox and click the link to verify your email.</p>
+      </div>
+    )
+  }
   return (
-    <div style={{ textAlign: "center", padding: "2rem" }}>
-      <h2>Verify Your Email</h2>
-      <p>We’ve sent you a verification email. Please check your inbox and click the link to verify your email.</p>
-    </div>
+    <Suspense>
+      <VerifyEmailComponent />
+    </Suspense>
   );
 };
 
